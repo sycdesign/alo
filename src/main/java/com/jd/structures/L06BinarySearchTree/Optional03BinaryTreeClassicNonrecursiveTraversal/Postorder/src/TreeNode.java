f83be9dc659c8336b768931150d9360689084002
gaohongbin5 <gaohongbin5@jd.com>
2020-08-21 14:53:06 +0800

    init:init project

---
+++ src/main/java/com/jd/structures/L06BinarySearchTree/Optional03BinaryTreeClassicNonrecursiveTraversal/Postorder/src/TreeNode.java
@@ -0,0 +1,9 @@
+package com.jd.structures.L06BinarySearchTree.Optional03BinaryTreeClassicNonrecursiveTraversal.Postorder.src;
+
+// Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
---
+++ src/main/java/com/jd/structures/L06BinarySearchTree/Optional05BinaryTreeMorrisTraversal/TreeNode.java
@@ -0,0 +1,9 @@
+package com.jd.structures.L06BinarySearchTree.Optional05BinaryTreeMorrisTraversal;
+
+// Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
