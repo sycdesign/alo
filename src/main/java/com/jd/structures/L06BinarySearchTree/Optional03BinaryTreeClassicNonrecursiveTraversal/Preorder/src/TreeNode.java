8b89b497664c946afa70b0680ac25ce87ec4fb74
gaohongbin5 <gaohongbin5@jd.com>
2020-08-25 18:08:09 +0800

    feat:structures

--- src/main/java/com/jd/structures/L06BinarySearchTree/Optional03BinaryTreeClassicNonrecursiveTraversal/Preorder/src/TreeNode.java
+++
@@ -1,9 +0,0 @@
-package com.jd.structures.L06BinarySearchTree.Optional03BinaryTreeClassicNonrecursiveTraversal.Preorder.src;
-
-// Definition for a binary tree node.
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
