b3319c1e0f8e015745453185cf83fe1bfa93980a
gaohongbin5 <gaohongbin5@jd.com>
2020-08-25 18:15:56 +0800

    feat:init project floder

---
+++ src/main/java/leetcode/editor/cn/P7ReverseInteger.java
@@ -0,0 +1,52 @@
+//给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。 
+//
+// 示例 1: 
+//
+// 输入: 123
+//输出: 321
+// 
+//
+// 示例 2: 
+//
+// 输入: -123
+//输出: -321
+// 
+//
+// 示例 3: 
+//
+// 输入: 120
+//输出: 21
+// 
+//
+// 注意: 
+//
+// 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231, 231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。 
+// Related Topics 数学 
+// 👍 2124 👎 0
+
+package leetcode.editor.cn;
+//Java：整数反转
+public class P7ReverseInteger{
+    public static void main(String[] args) {
+        Solution solution = new P7ReverseInteger().new Solution();
+        // TO TEST
+        System.out.println(Integer.MIN_VALUE);
+        solution.reverse(1534236469);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0){
+            int tmp = x % 10;
+            x /= 10;
+            if(result > Integer.MAX_VALUE/10 || (result == Integer.MAX_VALUE/10 && tmp>7)) return 0;
+            if(result < Integer.MIN_VALUE/10 || (result == Integer.MIN_VALUE/10 && tmp < -8)) return  0;
+            result = result * 10 + tmp;
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
