4c8746f113732d8f52c1f2c50fcf2733781dda10
gaohongbin5 <gaohongbin5@jd.com>
2020-09-15 14:48:18 +0800

    feat: format dy

---
+++ src/main/java/leetcode/editor/cn/P416PartitionEqualSubsetSum.java
@@ -0,0 +1,50 @@
+//给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。 
+//
+// 注意: 
+//
+// 
+// 每个数组中的元素不会超过 100 
+// 数组的大小不会超过 200 
+// 
+//
+// 示例 1: 
+//
+// 输入: [1, 5, 11, 5]
+//
+//输出: true
+//
+//解释: 数组可以分割成 [1, 5, 5] 和 [11].
+// 
+//
+// 
+//
+// 示例 2: 
+//
+// 输入: [1, 2, 3, 5]
+//
+//输出: false
+//
+//解释: 数组不能分割成两个元素和相等的子集.
+// 
+//
+// 
+// Related Topics 动态规划 
+// 👍 395 👎 0
+
+package leetcode.editor.cn;
+//Java：分割等和子集
+public class P416PartitionEqualSubsetSum{
+    public static void main(String[] args) {
+        Solution solution = new P416PartitionEqualSubsetSum().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean canPartition(int[] nums) {
+
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
