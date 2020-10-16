2050490e582f2b2344fac4d58b5393508e1a4758
gaohongbin5 <gaohongbin5@jd.com>
2020-10-16 17:14:32 +0800

    feat: 234 palindromeLinkedList

--- src/main/java/leetcode/editor/cn/P234PalindromeLinkedList.java
+++ src/main/java/leetcode/editor/cn/P234PalindromeLinkedList.java
@@ -22,14 +22,17 @@ package leetcode.editor.cn;
 //Java：回文链表
 public class P234PalindromeLinkedList{
     public static void main(String[] args) {
-        Solution solution = new P234PalindromeLinkedList().new Solution();
-        // TO TEST
+       /* Solution solution = new P234PalindromeLinkedList().new Solution();
+        ListNode n1 = new ListNode(0);
+        ListNode n2 = new ListNode(0);
+        n1.next = n2;
+        solution.isPalindrome(n1);*/
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for singly-linked list.
  */
-public class ListNode {
+public static class ListNode {
     int val;
     ListNode next;
     ListNode(int x) { val = x; }
@@ -37,7 +40,52 @@ public class ListNode {
 class Solution {
     public boolean isPalindrome(ListNode head) {
 
-        return false;
+        if (head == null) {
+            return true;
+        }
+
+        // Find the end of first half and reverse second half.
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // Check whether or not there is a palindrome.
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) {
+                result = false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        // Restore the list and return the result.
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    // Taken from https://leetcode.com/problems/reverse-linked-list/solution/
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
