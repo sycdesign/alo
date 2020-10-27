fba45e63365fccc2784b393ce1e49df614705716
gaohongbin5 <gaohongbin5@jd.com>
2020-10-27 20:57:24 +0800

    feat:P 167

--- src/main/java/com/jd/lee/L03UsingArray/L06TwoSumII/Solution1.java
+++ src/main/java/com/jd/lee/L03UsingArray/L06TwoSumII/Solution1.java
@@ -10,26 +10,35 @@ public class Solution1 {
 
   public int[] twoSum(int[] numbers, int target) {
 
-    if (numbers.length < 2 /*|| !isSorted(numbers)*/)
+    if (numbers.length < 2 /*|| !isSorted(numbers)*/) {
       throw new IllegalArgumentException("Illegal argument numbers");
+    }
 
-    for (int i = 0; i < numbers.length; i++)
-      for (int j = i + 1; j < numbers.length; j++)
+    for (int i = 0; i < numbers.length; i++) {
+      for (int j = i + 1; j < numbers.length; j++) {
         if (numbers[i] + numbers[j] == target) {
           int[] res = {i + 1, j + 1};
           return res;
         }
+      }
+    }
 
     throw new IllegalStateException("The input has no solution");
   }
 
   private boolean isSorted(int[] numbers) {
-    for (int i = 1; i < numbers.length; i++) if (numbers[i] < numbers[i - 1]) return false;
+    for (int i = 1; i < numbers.length; i++) {
+      if (numbers[i] < numbers[i - 1]) {
+        return false;
+      }
+    }
     return true;
   }
 
   private static void printArr(int[] nums) {
-    for (int num : nums) System.out.print(num + " ");
+    for (int num : nums) {
+      System.out.print(num + " ");
+    }
     System.out.println();
   }
 
--- src/main/java/com/jd/lee/L03UsingArray/L06TwoSumII/Solution2.java
+++ src/main/java/com/jd/lee/L03UsingArray/L06TwoSumII/Solution2.java
@@ -10,8 +10,9 @@ public class Solution2 {
 
   public int[] twoSum(int[] numbers, int target) {
 
-    if (numbers.length < 2 /*|| !isSorted(numbers)*/)
+    if (numbers.length < 2 /*|| !isSorted(numbers)*/) {
       throw new IllegalArgumentException("Illegal argument numbers");
+    }
 
     for (int i = 0; i < numbers.length - 1; i++) {
       int j = binarySearch(numbers, i + 1, numbers.length - 1, target - numbers[i]);
@@ -26,27 +27,42 @@ public class Solution2 {
 
   private int binarySearch(int[] nums, int l, int r, int target) {
 
-    if (l < 0 || l > nums.length) throw new IllegalArgumentException("l is out of bound");
+    if (l < 0 || l > nums.length) {
+      throw new IllegalArgumentException("l is out of bound");
+    }
 
-    if (r < 0 || r > nums.length) throw new IllegalArgumentException("r is out of bound");
+    if (r < 0 || r > nums.length) {
+      throw new IllegalArgumentException("r is out of bound");
+    }
 
     while (l <= r) {
       int mid = l + (r - l) / 2;
-      if (nums[mid] == target) return mid;
-      if (target > nums[mid]) l = mid + 1;
-      else r = mid - 1;
+      if (nums[mid] == target) {
+        return mid;
+      }
+      if (target > nums[mid]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
     }
 
     return -1;
   }
 
   private boolean isSorted(int[] numbers) {
-    for (int i = 1; i < numbers.length; i++) if (numbers[i] < numbers[i - 1]) return false;
+    for (int i = 1; i < numbers.length; i++) {
+      if (numbers[i] < numbers[i - 1]) {
+        return false;
+      }
+    }
     return true;
   }
 
   private static void printArr(int[] nums) {
-    for (int num : nums) System.out.print(num + " ");
+    for (int num : nums) {
+      System.out.print(num + " ");
+    }
     System.out.println();
   }
 
